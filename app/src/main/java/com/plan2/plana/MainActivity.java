dd0c20086929c06687c70b2edc8c766dc02d53a6
Park JongBeom <qk54r71@nate.com>
2017-09-05 14:28:35 +0900

    Hello World

---
+++ app/src/main/java/com/plan2/plana/MainActivity.java
@@ -0,0 +1,16 @@
+package com.plan2.plana;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        System.out.println("Hello.world");
+
+    }
+}
